e8c1c05e8de161192d8be0da02922020a5ee897e
NguyenDuy2605 <www.hoangduy260503@gmail.com>
2026-06-04 15:29:06 +0700

    rebuild main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import List
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 import os
 from langchain_anthropic import ChatAnthropic
@@ -7,10 +9,18 @@ from langchain_core.messages import HumanMessage
 from langchain_tavily import TavilySearch
 load_dotenv()
 
+class Source(BaseModel):
+    """ Schema for a source used by the agent"""
+    url: str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """ Schema for agent response with answer and sources"""
+    answer: str = Field(description="The agent's answer to the query")
+    source: List[Source] = Field(default_factory=list, description="List of sources used to generate the answer")
 
 llm = ChatAnthropic(model_name="claude-sonnet-4-6", temperature=0)
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 def main():
     print("Hello from langchain-course!")
